5f30ac19f7c6a923ce225699430694262e5ecbc4
Kamie Short <kamie.short@gmail.com>
2022-02-14 12:20:57 -0800

    sign up user is funcitoning, redirect functioning

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
-const signInForm = document.getElementById('sign-in');
-const signInEmail = document.getElementById('sign-in-email');
-const signInPassword = document.getElementById('sign-in-password');
+import { redirectIfLoggedIn, signupUser } from './fetch-utils.js';
+
+// const signInForm = document.getElementById('sign-in');
+// const signInEmail = document.getElementById('sign-in-email');
+// const signInPassword = document.getElementById('sign-in-password');
 
 const signUpForm = document.getElementById('sign-up');
 const signUpEmail = document.getElementById('sign-up-email');
 const signUpPassword = document.getElementById('sign-up-password');
 
 // Wire up sign in and sign up forms to supabase
+redirectIfLoggedIn();
 
-sign up from event listener --async 
-e.prevent
+signUpForm.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    console.log(signUpEmail.value, signUpPassword.value);
+    await signupUser(signUpEmail.value, signUpPassword.value);
+    redirectIfLoggedIn();
+});
+
 // Redirect to /other-page on successful auth
 // Redirect to /other-page when page loads if user is authenticated
--- other-page/other-page.js
+++ other-page/other-page.js
@@ -1,2 +1,8 @@
 // use checkAuth function to redirect is user not authenticated
+
+import { checkAuth } from '../fetch-utils';
+
 // add event listener to the logout button and call logout
+// checkAuth
+
+// const logoutBtn
